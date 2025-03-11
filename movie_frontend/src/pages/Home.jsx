e3dc944f5d3ed7a50ab2fbc763ea15af7aad08f0
DaryanZabear <71202670+Daiyan-Zubaier@users.noreply.github.com>
2025-03-11 02:52:25 -0400

    Added state changes to search query in the home page

--- movie_frontend/src/pages/Home.jsx
+++ movie_frontend/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import MovieCard from "../components/MovieCard"
+import { useState } from "react"
 
 function Home(){
+    const [searchQuery, setSearchQuery] = useState("");
     const movies = [
         {id: 1, title: "The Shawshank Redemption", year: 1994},
         {id: 2, title: "The Godfather", year: 1972},    
@@ -9,20 +11,26 @@ function Home(){
     ]
 
     const handleSubmit = (event) => {
-        event.preventDefault()
-        console.log("Search form submitted")
-    }
+        // Normally clears page during submission, prevent default just prevents that
+        event.preventDefault();
+        console.log("Search form submitted");
+    };
 
     return (
         <div className="home">
             <form onSubmit={handleSubmit} className="search-form">
-                <input type="text" placeholder="Search..." 
-                className="search-bar"></input>
-                <button type="submit">Search</button>
+                <input type="text" 
+                placeholder="Search..." 
+                className="search-bar"
+                value={searchQuery}
+                // Listens for a change in in state, and updates the searchQuery to the new value
+                onChange={(e) => setSearchQuery(e.target.value)}></input>
+                <button type="submit">Search</button> 
             </form>
 
             <div className="movies-grid">
-                {movies.map(movie => (
+                {movies.map(movie => 
+                    movie.title.toLocaleLowerCase().startsWith(searchQuery) && (
                     <MovieCard key={movie.id} movie={movie} />
                 ))}
             </div>

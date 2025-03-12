482fdb4d8bf82d5515993ff36bec2d6a9d041875
DaryanZabear <71202670+Daiyan-Zubaier@users.noreply.github.com>
2025-03-11 23:38:09 -0400

    Added basic structure for watched page

---
+++ movie_frontend/src/pages/Watched.jsx
@@ -0,0 +1,11 @@
+function Watched(){
+    return (
+    <div className="favourites-empty">
+        <h2>Nothing Watched Yet</h2>
+        <p>Go back and add some movies to your watched list!</p>
+    </div>
+    )
+
+}
+
+export default Watched
